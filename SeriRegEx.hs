7d51cd47b00e43a6793922f9362580ce53a12770
Richard Uhler <ruhler@csail.mit.edu>
2012-12-14 11:27:25 -0500

    Removed unused Star construct from RegEx.

--- SeriRegEx.hs
+++ SeriRegEx.hs
@@ -10,7 +10,6 @@ data RegEx c =
            | Empty          -- never matches
            | Atom c
            | Range c c
-           | Star (RegEx c)
            | Concat (RegEx c) (RegEx c)
            | Or (RegEx c) (RegEx c)
    deriving(Eq)
@@ -25,7 +24,6 @@ match (Atom x) [c] = x == c
 match (Atom _) _ = False
 match (Range cmin cmax) [c] = cmin <= c && c <= cmax
 match (Range _ _) _ = False
-match s@(Star x) str = match (Or Epsilon (Concat x s)) str
 match (Concat a b) str = any (matchboth a b) (partitions str)
 match (Or a b) str = match a str || match b str
 
