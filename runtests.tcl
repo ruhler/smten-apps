c8a18ca6221e14715cd0d1da39787bb0bd102679
Richard Uhler <ruhler@csail.mit.edu>
2012-12-18 11:36:35 -0500

    Implemented match... It doesn't work right yet.

--- runtests.tcl
+++ runtests.tcl
@@ -3,6 +3,6 @@ set ::hampi [lindex $argv 0]
 
 foreach x [glob "tests/*.hmp"] {
     puts -nonewline "$x: "
-    catch {exec $::hampi $x 3 ">@" stdout "2>@" stdout}
+    catch {exec $::hampi $x 10 ">@" stdout "2>@" stdout}
 }
     
