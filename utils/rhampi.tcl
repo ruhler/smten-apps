550b0ef26065b32ee49e7741d614824bde4afeca
Richard Uhler <ruhler@csail.mit.edu>
2013-05-29 07:16:16 -0400

    Re-organized test cases.

--- utils/rhampi.tcl
+++ utils/rhampi.tcl
@@ -1,10 +1,8 @@
 
 set files $argv
 
-#exec ./utils/rhampi_s &
 foreach x $files {
     puts -nonewline "$x: "
     exec ./utils/rhampi_c $x ">@" stdout "2>@" stdout
 }
-exec ./utils/rhampi_shutdown
 
