ac6c813d1041c10bd551487c250e806dc1d60e0a
Richard Uhler <ruhler@csail.mit.edu>
2013-06-11 16:55:27 -0400

    Revert "Updated for new handling of primitives in smten."
    This reverts commit 37eb2d5eb3f09dd5456b1264a7aa2e99c97c4bfe.

--- src/Imports.hs
+++ src/Imports.hs
@@ -1,47 +1,23 @@
 
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-
 module Imports (
-    getArgs, exitSuccess, readFile, parseHampi, read,
+    getArgs, exitSuccess, readFile, parseHampi, timeout, read,
+    IO, Char, Integer, Maybe, Either, Hampi,
     ) where
 
-import qualified Prelude as P
-import Prelude hiding (read, readFile)
-import qualified System.Environment as P (getArgs)
-import qualified System.Timeout as P
-import qualified System.Exit as P
+import System.Environment (getArgs)
+import qualified System.Timeout as S
+import System.Exit
 
 import Control.Monad.State
 
-import Smten.Runtime.SmtenHS (Haskelly(..), SmtenHS0, SmtenHS1)
-import qualified Smten.Runtime.SmtenHS as S
-import qualified Smten.Runtime.IO as S
-
 import Hampi
-import qualified Grammar as P
+import qualified Grammar as S
 
-getArgs :: (Haskelly (IO [String]) x) => x
-getArgs = {-# SCC "IMPORT_getArgs" #-} frhs P.getArgs
-
---timeout :: (Haskelly Integer i, SmtenHS0 i,
---            Haskelly (IO a) io, SmtenHS ioa, SmtenHS0 ioma,
---            Haskelly (IO (Maybe a)) ioma) => i -> io a -> io (m a)
---timeout i x = frhs (P.timeout (fromInteger (tohs i :: Maybe a)) (tohs x))
-
-exitSuccess :: (Haskelly () u, SmtenHS0 u) => S.IO u
-exitSuccess = {-# SCC "IMPORT_exitSuccess" #-} frhs (P.exitSuccess :: P.IO ())
-
-readFile :: (Haskelly String string, SmtenHS0 string) => string -> S.IO string
-readFile = {-# SCC "IMPORT_readFile" #-} frhs P.readFile
-
-read :: (Haskelly String string, SmtenHS0 string) => string -> S.Integer
-read = {-# SCC "IMPORT_read" #-} frhs (P.read :: String -> Integer)
+timeout :: Integer -> IO a -> IO (Maybe a)
+timeout = S.timeout . fromInteger
     
-parseHampi :: (Haskelly String string,
-               Haskelly (Either String Hampi) x) => string -> x
-parseHampi s = {-# SCC "IMPORT_parseHampi" #-}
-    frhs $ case runStateT P.parseHampi (tohs s) of
-               Left msg -> Left msg
-               Right v -> Right (fst v)
+parseHampi :: String -> Either String Hampi
+parseHampi s = case runStateT S.parseHampi s of
+                 Left msg -> Left msg
+                 Right v -> Right (fst v)
 
