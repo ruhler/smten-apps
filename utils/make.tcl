4772f0bda48d8d6656075bec1319d25c25623e0c
Richard Uhler <ruhler@csail.mit.edu>
2013-06-13 16:58:45 -0400

    Update import for Smten.Bit.

--- utils/make.tcl
+++ utils/make.tcl
@@ -13,9 +13,9 @@ proc hrun {args} {
 hrun mkdir -p build
 hrun happy src/Grammar.y -o build/Grammar.hs
 run smten --include src --hsdir build -f src/shampi.smtn
-hrun ghc -o build/shampi -O0 -hidir build/ -odir build/ -isrc -ibuild --make build/Smten/Lib/Main.hs -main-is Smten.Lib.Main.main__ -rtsopts -fno-warn-overlapping-patterns
+hrun ghc -o build/shampi -O2 -hidir build/ -odir build/ -isrc -ibuild --make build/Smten/Lib/Main.hs -main-is Smten.Lib.Main.main__ -rtsopts -fno-warn-overlapping-patterns
 
-hrun ghc -o build/shampi_prof -osuf o_prof -O0 -hidir build/ -odir build/ -isrc -ibuild --make build/Smten/Lib/Main.hs -main-is Smten.Lib.Main.main__ -rtsopts -prof -fno-warn-overlapping-patterns
+hrun ghc -o build/shampi_prof -osuf o_prof -O2 -hidir build/ -odir build/ -isrc -ibuild --make build/Smten/Lib/Main.hs -main-is Smten.Lib.Main.main__ -rtsopts -prof -fno-warn-overlapping-patterns
 
 run tclsh utils/runtests.tcl ./build/shampi > build/tests.shampi
 hrun diff utils/tests.rhampi build/tests.shampi
