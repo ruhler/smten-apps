94b19e412b8dadf43b180992ac870431933e424f
Richard Uhler <ruhler@csail.mit.edu>
2013-10-23 10:26:48 -0400

    Implemented support for generator functions.

--- sketch/src/Generate.hs
+++ sketch/src/Generate.hs
@@ -17,20 +17,24 @@ import Sketch
 -- candidate program without holes.
 generate :: Prog -> Symbolic Prog
 generate p = do
-  let readed = runReaderT (mapM genD p) (GR (error "no top level output type"))
+  let env = Map.fromList [(d_name d, d) | d <- p]
+      readed = runReaderT (mapM genD p) (GR env (error "no top level output type"))
   (ds, s) <- runStateT readed (TS (error "TODO: generate with global tyenv") [])
   return $ ts_decls s ++ ds
 
 type TypeEnv = Map.Map Name Type
 
 data GR = GR {
+    -- | The program environment.
+    gr_env :: Map.Map Name Decl,
+
     -- | The target output type.
     gr_oty :: Type
 }
 
 data TS = TS {
     -- | The type environment.
-    ts_env :: TypeEnv,
+    ts_tyenv :: TypeEnv,
 
     -- | Emmitted (generated) declarations
     ts_decls :: [Decl]
@@ -41,8 +45,14 @@ type GM = ReaderT GR (StateT TS Symbolic)
 withty :: Type -> GM a -> GM a
 withty t = local (\r -> r { gr_oty = t })
 
-emit :: Decl -> GM ()
-emit d = modify $ \s -> s { ts_decls = d : ts_decls s }
+-- Emit a declaration. Use a template for the declaration name, and a
+-- uniquified version of that name will be used instead and returned. 
+emit :: Decl -> GM Name
+emit d = do
+    s <- get
+    let nm = d_name d ++ "_" ++ show (length $ ts_decls s)
+    put $ s { ts_decls = d { d_name = nm } : ts_decls s }
+    return nm
 
 liftSymbolic :: Symbolic a -> GM a
 liftSymbolic = lift . lift
@@ -52,9 +62,11 @@ genD d@(VarD {}) = return d
 genD d@(FunD {}) = do
     let tyenv = Map.fromList (map swap . f_args . fd_val $ d)
         oty = f_outty . fd_val $ d
-    modify $ \s -> s { ts_env = tyenv }
+    tyenvold <- gets ts_tyenv
+    modify $ \s -> s { ts_tyenv = tyenv }
     body' <- withty oty $ genS (f_body . fd_val $ d)
     let val' = (fd_val d) { f_body = body' }
+    modify $ \s -> s { ts_tyenv = tyenvold }
     return $ d { fd_val = val' }
 
 genS :: Stmt -> GM Stmt
@@ -62,12 +74,12 @@ genS (ReturnS x) = ReturnS <$> genE x
 genS (AssertS x) = AssertS <$> withty BitT (genE x)
 genS (RepeatS n s) = liftM2 RepeatS (withty IntT (genE n)) (genS s)
 genS s@(DeclS ty nm) = do
-  env <- gets ts_env
+  env <- gets ts_tyenv
   let env' = Map.insert nm ty env
-  modify $ \s -> s { ts_env = env' }
+  modify $ \s -> s { ts_tyenv = env' }
   return s
 genS (UpdateS nm e) = do
-  env <- gets ts_env
+  env <- gets ts_tyenv
   case Map.lookup nm env of
     Nothing -> error $ "variable " ++ nm ++ " not in scope"
     Just ty -> UpdateS nm <$> (withty ty (genE e))
@@ -116,7 +128,14 @@ genE (AccessE a b) = do
     b' <- withty IntT (genE b)
     return (AccessE a' b')
 genE (CastE t e) = CastE t <$> withty UnknownT (genE e)
-genE (AppE f xs) = liftM2 AppE (withty UnknownT $ genE f)
-                               (withty UnknownT $ mapM genE xs)
+genE (AppE f xs) = do
+    env <- asks gr_env
+    case f of
+       VarE gnm | Just (FunD _ gf GeneratorF) <- Map.lookup gnm env -> do
+           generated <- genD (FunD gnm gf NormalF)
+           gnm' <- emit generated
+           AppE (VarE gnm') <$> (withty UnknownT $ mapM genE xs)
+       _ -> liftM2 AppE (withty UnknownT $ genE f)
+                        (withty UnknownT $ mapM genE xs)
 genE x@(FunE f) = return x
 
