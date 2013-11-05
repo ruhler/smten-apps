8b7c0f613cb5fb53857fd8ccd36aac85b65cf059
Richard Uhler <ruhler@csail.mit.edu>
2013-11-05 16:57:35 -0500

    Remove unnecessary type tracking in Generate phase.

--- sketch/src/Generate.hs
+++ sketch/src/Generate.hs
@@ -15,7 +15,7 @@ import Sketch
 -- candidate program without holes.
 generate :: ProgEnv -> Symbolic Prog
 generate p = do
-  let readed = runReaderT (mapM genD (declsof p)) (GR p (error "no top level output type"))
+  let readed = runReaderT (mapM genD (declsof p)) (GR p)
   (ds, s) <- runStateT readed (TS (error "TODO: generate with global tyenv") [])
   return $ ts_decls s ++ ds
 
@@ -23,10 +23,7 @@ type TypeEnv = Map.Map Name Type
 
 data GR = GR {
     -- | The program environment.
-    gr_env :: ProgEnv,
-
-    -- | The target output type.
-    gr_oty :: Type
+    gr_env :: ProgEnv
 }
 
 data TS = TS {
@@ -38,9 +35,6 @@ data TS = TS {
 }
 
 type GM = ReaderT GR (StateT TS Symbolic)
-
-withty :: Type -> GM a -> GM a
-withty t = local (\r -> r { gr_oty = t })
 
 -- Emit a declaration. Use a template for the declaration name, and a
 -- uniquified version of that name will be used instead and returned. 
@@ -57,18 +51,18 @@ liftSymbolic = lift . lift
 genD :: Decl -> GM Decl
 genD d@(VarD {}) = return d
 genD d@(FunD {}) = do
-    let FunT oty argtys = f_type . fd_val $ d
+    let FunT _ argtys = f_type . fd_val $ d
         tyenv = Map.fromList (zip (f_args . fd_val $ d) argtys)
     tyenvold <- gets ts_tyenv
     modify $ \s -> s { ts_tyenv = tyenv }
-    body' <- withty oty $ genS (f_body . fd_val $ d)
+    body' <- genS (f_body . fd_val $ d)
     let val' = (fd_val d) { f_body = body' }
     modify $ \s -> s { ts_tyenv = tyenvold }
     return $ d { fd_val = val' }
 
 genS :: Stmt -> GM Stmt
 genS (ReturnS x) = ReturnS <$> genE x
-genS (AssertS x) = AssertS <$> withty BitT (genE x)
+genS (AssertS x) = AssertS <$> genE x
 genS (RepeatS en s) = do
   -- TODO: evaluate n statically as much as possible here
   -- Perhaps a 'simplify' operation on expressions would be useful
@@ -78,7 +72,7 @@ genS (RepeatS en s) = do
   let count = case en of
                 ValE (IntV v) -> v
                 _ -> bnd_unroll_amnt
-  en' <- withty IntT $ genE en
+  en' <- genE en
   let unroll n
         | n >= count = blockS []
         | otherwise =
@@ -87,9 +81,9 @@ genS (RepeatS en s) = do
             in IfS (GtE en' (ValE (IntV n))) ifthen ifelse
   genS (unroll 0)
 
-genS (WhileS c s) = liftM2 WhileS (withty BitT (genE c)) (genS s)
+genS (WhileS c s) = liftM2 WhileS (genE c) (genS s)
 genS (ForS init cond incr body) =
-    liftM4 ForS (genS init) (withty BitT (genE cond)) (genS incr) (genS body)
+    liftM4 ForS (genS init) (genE cond) (genS incr) (genS body)
 genS s@(DeclS ty nm) = do
   env <- gets ts_tyenv
   let env' = Map.insert nm ty env
@@ -101,44 +95,32 @@ genS (UpdateS nm e) = do
     Nothing -> error $ "variable " ++ nm ++ " not in scope"
     Just ty@(ArrT {}) -> do
         -- insert an explicit cast here so padding is performed as needed.
-        e' <- withty ty (genE e)
+        e' <- genE e
         return $ UpdateS nm (CastE ty e')
-    Just ty -> UpdateS nm <$> (withty ty (genE e))
-genS (ArrUpdateS nm idx e) = do
-    env <- gets ts_tyenv
-    let t = case Map.lookup nm env of
-                Just (ArrT t _) -> t
-                Just _ -> error $ "variable " ++ nm ++ " is not an array"
-                Nothing -> error $ "variable " ++ nm ++ " not in scope"
-    idx' <- withty IntT $ genE idx
-    e' <- withty t $ genE e
-    return (ArrUpdateS nm idx' e')
-genS (IfS p a b) = liftM3 IfS (withty BitT $ genE p) (genS a) (genS b)
+    Just ty -> UpdateS nm <$> (genE e)
+genS (ArrUpdateS nm idx e) = liftM2 (ArrUpdateS nm) (genE idx) (genE e)
+genS (IfS p a b) = liftM3 IfS (genE p) (genS a) (genS b)
 genS (BlockS xs) = blockS <$> mapM genS xs
 
 genE :: Expr -> GM Expr
 genE (AndE a b) = liftM2 AndE (genE a) (genE b)
 genE (AddE a b) = liftM2 AddE (genE a) (genE b)
 genE (SubE a b) = liftM2 SubE (genE a) (genE b)
-genE (LtE a b) = liftM2 LtE (withty IntT $ genE a) (withty IntT $ genE b)
-genE (GtE a b) = liftM2 GtE (withty IntT $ genE a) (withty IntT $ genE b)
-genE (LeE a b) = liftM2 LeE (withty IntT $ genE a) (withty IntT $ genE b)
-genE (GeE a b) = liftM2 GeE (withty IntT $ genE a) (withty IntT $ genE b)
-genE (EqE a b) = withsamety EqE a b
-genE (NeqE a b) = withsamety NeqE a b
-genE (ArrayE a) = do
-    ty <- asks gr_oty
-    case ty of
-        ArrT t _ -> ArrayE <$> withty t (mapM genE a)
-        _ -> ArrayE <$> withty UnknownT (mapM genE a)
+genE (LtE a b) = liftM2 LtE (genE a) (genE b)
+genE (GtE a b) = liftM2 GtE (genE a) (genE b)
+genE (LeE a b) = liftM2 LeE (genE a) (genE b)
+genE (GeE a b) = liftM2 GeE (genE a) (genE b)
+genE (EqE a b) = liftM2 EqE (genE a) (genE b)
+genE (NeqE a b) = liftM2 NeqE (genE a) (genE b)
+genE (ArrayE a) = ArrayE <$> mapM genE a
 genE (XorE a b) = liftM2 XorE (genE a) (genE b)
 genE (MulE a b) = liftM2 MulE (genE a) (genE b)
 genE (ModE a b) = liftM2 ModE (genE a) (genE b)
 genE (OrE a b) = liftM2 OrE (genE a) (genE b)
-genE (LOrE a b) = liftM2 LOrE (withty BitT $ genE a) (withty BitT $ genE b)
-genE (LAndE a b) = liftM2 LAndE (withty BitT $ genE a) (withty BitT $ genE b)
-genE (ShlE a b) = liftM2 ShlE (genE a) (withty IntT $ genE b)
-genE (ShrE a b) = liftM2 ShrE (genE a) (withty IntT $ genE b)
+genE (LOrE a b) = liftM2 LOrE (genE a) (genE b)
+genE (LAndE a b) = liftM2 LAndE (genE a) (genE b)
+genE (ShlE a b) = liftM2 ShlE (genE a) (genE b)
+genE (ShrE a b) = liftM2 ShrE (genE a) (genE b)
 genE (NotE a) = NotE <$> genE a
 genE (HoleE ty bnd) = do
   env <- asks gr_env
@@ -152,82 +134,19 @@ genE (BitChooseE ty a b) = do
   return (OrE (AndE a' x) (AndE b' (NotE x)))
 genE x@(ValE {}) = return x
 genE x@(VarE {}) = return x
-genE (AccessE a b) = do
-    a' <- withty UnknownT $ genE a
-    b' <- withty IntT (genE b)
-    return (AccessE a' b')
-genE (CastE t e) = CastE t <$> withty UnknownT (genE e)
-genE (ICastE s d e) = ICastE s d <$> withty s (genE e)
+genE (AccessE a b) = liftM2 AccessE (genE a) (genE b)
+genE (CastE t e) = CastE t <$> genE e
+genE (ICastE s d e) = ICastE s d <$> genE e
 genE (AppE fnm xs) = do
     env <- asks gr_env
     case Map.lookup fnm env of
        Just (FunD _ f kind) -> do
           let FunT _ txs = f_type f
-          xs' <- sequence [withty t (genE x) | (t, x) <- zip txs xs]
+          xs' <- mapM genE xs
           fnm' <- case kind of
                  GeneratorF -> do
                     generated <- genD (FunD fnm f NormalF)
                     emit generated
                  _ -> return fnm
           return $ AppE fnm' xs'
-
--- Generate a binary operator, where the only thing we know about the operand
--- types is that they must be the same.
-withsamety :: (Expr -> Expr -> Expr) -> Expr -> Expr -> GM Expr
-withsamety f a b = do
-    t <- liftM2 unify (typeof a) (typeof b)
-    liftM2 f (withty t $ genE a) (withty t $ genE b)
-
--- Determine as best as possible the type of the given expression.
--- Returns 'UnknownT' if the type is not certain.
-typeof :: Expr -> GM Type
-typeof (ValE v) = return $ typeofV v
-typeof (AndE a b) = liftM2 unify (typeof a) (typeof b)
-typeof (AddE a b) = liftM2 unify (typeof a) (typeof b)
-typeof (SubE a b) = liftM2 unify (typeof a) (typeof b)
-typeof (LtE a b) = return BitT
-typeof (GtE a b) = return BitT
-typeof (LeE a b) = return BitT
-typeof (GeE a b) = return BitT
-typeof (EqE a b) = return BitT
-typeof (NeqE a b) = return BitT
-typeof (ArrayE a) = return UnknownT
-typeof (XorE a b) = liftM2 unify (typeof a) (typeof b)
-typeof (MulE a b) = return IntT
-typeof (ModE a b) = return IntT
-typeof (OrE a b) = liftM2 unify (typeof a) (typeof b)
-typeof (LOrE a b) = return BitT
-typeof (LAndE a b) = return BitT
-typeof (ShlE a b) = typeof a
-typeof (ShrE a b) = typeof a
-typeof (NotE a) = typeof a
-typeof (HoleE ty bnd) = return ty
-typeof (BitChooseE ty a b) = return ty
-typeof (VarE nm) = do
-    env <- asks gr_env
-    tyenv <- gets ts_tyenv
-    case Map.lookup nm tyenv of
-        Just v -> return v
-        Nothing -> case Map.lookup nm env of    
-                      Just d -> return (d_type d)
-                      Nothing -> return UnknownT
-typeof (AccessE a b) = do
-    ta <- typeof a
-    case ta of
-        ArrT t _ -> return t
-        _ -> return UnknownT
-typeof (CastE t e) = return t
-typeof (ICastE s d e) = return d
-typeof (AppE nm xs) = do
-    env <- asks gr_env
-    case Map.lookup nm env of
-       Just (FunD _ (Function (FunT v _) _ _) _) -> return v
-       Nothing -> return UnknownT
-
--- Try to unify the given types.
-unify :: Type -> Type -> Type
-unify UnknownT t = t
-unify t UnknownT = t
-unify (ArrT a x) (ArrT b _) = ArrT (unify a b) x
-unify x _ = x
 
