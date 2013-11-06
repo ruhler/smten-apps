afd27768209d44f1e7bca2235e05431e89c83c35
Richard Uhler <ruhler@csail.mit.edu>
2013-11-06 18:18:17 -0500

    Improved command line parser.
    In preparation for supporting more command line arguments.

--- sketch/src/sketch.hs
+++ sketch/src/sketch.hs
@@ -15,14 +15,45 @@ import Sketch
 import Static
 import Synthesis
 
+usage :: String
+usage = unlines [
+    "Usage:",
+    "  sketch FILE [-d debug.dbg]",
+    ""]
+
+data CmdArgs = CmdArgs {
+    cmd_err :: Maybe String,
+    cmd_file :: Maybe String,
+    cmd_debug :: Maybe String
+}
+
+defargs :: CmdArgs
+defargs = CmdArgs {
+    cmd_err = Nothing,
+    cmd_file = Nothing,
+    cmd_debug = Nothing
+}
+
+parseargs :: [String] -> CmdArgs
+parseargs s =
+  case s of
+    [] -> defargs
+    ("-d" : dbg : rest) -> (parseargs rest) { cmd_debug = Just dbg }
+    (('-':f) : _) -> defargs { cmd_err = Just ("unrecognized flag: " ++ f) }
+    (f : rest) -> (parseargs rest) { cmd_file = Just f }
+
 main :: IO ()
 main = do
-  args <- getArgs
-  (fin, dbg) <- case args of 
-                 [f, "-d", df] -> return (f, Just df)
-                 [f] -> return (f, Nothing)
-                 _ -> error "usage: sketch FILE [-d debug.dbg]"
-  input <- readFile fin
+  argv <- getArgs
+  let args = parseargs argv
+  case cmd_err args of
+     Just err -> error $ unlines [err, usage]
+     Nothing -> return ()
+
+  input <- case cmd_file args of
+              Just fin -> readFile fin
+              Nothing -> error $ unlines ["no input file", usage]
+    
   sk <- case evalStateT parseSketch input of
            Left msg -> fail msg
            Right x -> return x
@@ -31,9 +62,10 @@ main = do
   let st = static (envof sk)
   --putStrLn $ "POST STATIC: " ++ show st
 
-  solver <- case dbg of
+  solver <- case cmd_debug args of
                Just fnm -> debug fnm yices2
                Nothing -> return yices2
+
   syn <- runSMT solver (synthesize defaultOptions (envof st))
   case syn of
     Nothing -> fail "sketch not satisfiable"
