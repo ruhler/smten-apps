fb4571b39d6996eadb8401bed35362f74f3b811b
Richard Uhler <ruhler@csail.mit.edu>
2014-01-16 12:12:25 -0500

    Reimplemented EvalMonad to be a proper error monad.

--- sketch/src/EvalMonad.hs
+++ sketch/src/EvalMonad.hs
@@ -9,60 +9,69 @@ module EvalMonad (
 import Smten.Prelude
 import Smten.Control.Monad.State
 import qualified Smten.Data.Map as Map
+import Smten.Data.Functor
 import Sketch
 
-data SS = SS {
-    -- | The Global Environment
-    ss_env :: Map.Map Name Decl,
+type Env = Map.Map Name Decl
+type Vars = Map.Map Name Value
 
+data SS = SS {
     -- | Local variables.
     ss_vars :: Map.Map Name Value,
 
     -- | The output of the statement if any.
-    ss_out :: Value,
-
-    -- | Predicate which says if the result is valid.
-    ss_valid :: Bool
+    ss_out :: Value
 }
 
 -- The evaluation monad.
 --  It has access to a read-only global environment,
 --  it has access to a read/write local environment,
+--  it has access to a read/write output value,
 --  and it can fail.
-type EvalM = State SS
+newtype EvalM a = EvalM {
+   runEvalM_ :: Env -> SS -> Maybe (a, SS)
+}
+
+instance Functor EvalM where
+  fmap f x = x >>= (return . f)
+
+instance Monad EvalM where
+  return x = EvalM $ \_ s -> return (x, s)
+  (>>=) x f = EvalM $ \e s -> do
+      (v, s') <- runEvalM_ x e s
+      runEvalM_ (f v) e s'
 
 runEvalM :: ProgEnv -> EvalM a -> Maybe a
-runEvalM env q = 
-  case runState q (SS env Map.empty (error "evalM.ss_out._|_") True) of
-    (v, s) -> if (ss_valid s) then Just v else Nothing
+runEvalM env q =
+  let s0 = SS Map.empty (error "evalM.ss_out._|_")
+  in fst <$> runEvalM_ q env s0
 
 -- Evaluate the monad in the given scope.
 -- Outer scopes are not visible.
 scope :: Map.Map Name Value -> EvalM a -> EvalM a
-scope vars x = do
-  olds <- get
-  put (SS (ss_env olds) vars (error "scope.ss_out._|_") (ss_valid olds))
-  r <- x
-  modify $ \s -> s { ss_out = ss_out olds, ss_vars = ss_vars olds }
-  return r
+scope vars x = EvalM $ \e s -> do
+  r <- runEvalM_ x e (SS vars (error "EvalM: no output value"))
+  return (fst r, s)
 
 assert :: Bool -> EvalM ()
-assert p = modify $ \s -> s { ss_valid = ss_valid s && p }
+assert True = return ()
+assert False = EvalM $ \_ _ -> Nothing
 
 -- Look for the given declaration in the program environment.
 lookupDecl :: Name -> EvalM (Maybe Decl)
-lookupDecl nm = gets (Map.lookup nm . ss_env)
+lookupDecl nm = EvalM $ \e s -> return (Map.lookup nm e, s)
 
 -- Look for the given variable in the local scope
 lookupVar :: Name -> EvalM (Maybe Value)
-lookupVar nm = gets (Map.lookup nm . ss_vars)
+lookupVar nm = EvalM $ \_ s -> return (Map.lookup nm (ss_vars s), s)
 
 insertVar :: Name -> Value -> EvalM ()
-insertVar nm val = modify $ \s -> s { ss_vars = Map.insert nm val (ss_vars s) }
+insertVar nm val = EvalM $ \_ s ->
+    return ((), s { ss_vars = Map.insert nm val (ss_vars s)})
 
 getOutput :: EvalM Value
-getOutput = gets ss_out
+getOutput = EvalM $ \_ s -> return (ss_out s, s)
 
 setOutput :: Value -> EvalM ()
-setOutput v = modify $ \s -> s { ss_out = v }
+setOutput v = EvalM $ \_ s -> return ((), s { ss_out = v })
 
