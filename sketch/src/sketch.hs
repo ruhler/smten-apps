1904f489683a0a9bc0fdac981740a7ea5e52ee8c
Richard Uhler <ruhler@csail.mit.edu>
2014-01-30 14:06:25 -0500

    Accept multiple files on the command line.
    This makes it easier to profile a bunch of tests all in one batch.

--- sketch/src/sketch.hs
+++ sketch/src/sketch.hs
@@ -23,17 +23,19 @@ import Synthesis
 usage :: String
 usage = unlines [
     "Usage:",
-    "  sketch FILE [OPTIONS]",
+    "  sketch [OPTIONS] [FILE...]",
     "",
     " Options:",
     "    -d FILE            Output debug info to the given file",
+    "    -s solver          Select the back-end solver to use:",
+    "                         yices1, yices2, z3, stp, or minisat",
     "    --bnd-cbits N      Set the bnd-cbits value to N",
     "    --bnd-inbits N     Set the bnd-inbits value to N",
     ""]
 
 data CmdArgs = CmdArgs {
     cmd_err :: Maybe String,
-    cmd_file :: Maybe String,
+    cmd_files :: [String],
     cmd_debug :: Maybe String,
     cmd_solver :: Solver,
     cmd_opts :: Options
@@ -42,7 +44,7 @@ data CmdArgs = CmdArgs {
 defargs :: CmdArgs
 defargs = CmdArgs {
     cmd_err = Nothing,
-    cmd_file = Nothing,
+    cmd_files = [],
     cmd_debug = Nothing,
     cmd_solver = yices2,
     cmd_opts = defaultOptions
@@ -68,7 +70,9 @@ parseargs s =
             opts' = (cmd_opts args) { bnd_inbits = read n }
         in args { cmd_opts = opts' }
     (('-':f) : _) -> defargs { cmd_err = Just ("unrecognized flag: " ++ f) }
-    (f : rest) -> (parseargs rest) { cmd_file = Just f }
+    (f : rest) ->
+       let x = parseargs rest
+       in x { cmd_files = f : (cmd_files x) }
 
 main :: IO ()
 main = do
@@ -78,24 +82,23 @@ main = do
      Just err -> error $ unlines [err, usage]
      Nothing -> return ()
 
-  input <- case cmd_file args of
-              Just fin -> readFile fin
-              Nothing -> error $ unlines ["no input file", usage]
-    
-  sk <- case evalStateT parseSketch input of
-           Left msg -> fail msg
-           Right x -> return x
-  --putStrLn $ "POST PARSE: " ++ show sk
-
-  let st = static (envof sk)
-  --putStrLn $ "POST STATIC: " ++ show st
-
   solver <- case cmd_debug args of
                Just fnm -> debug fnm (cmd_solver args)
                Nothing -> return (cmd_solver args)
 
-  syn <- runSMT solver (synthesize (cmd_opts args) (envof st))
-  case syn of
-    Nothing -> fail "sketch not satisfiable"
-    Just v -> putStrLn (pretty v)
+  flip mapM_ (cmd_files args) $ \fin -> do
+    putStrLn $ "Running sketch on " ++ show fin ++ "..."
+    input <- readFile fin
+    sk <- case evalStateT parseSketch input of
+             Left msg -> fail msg
+             Right x -> return x
+    --putStrLn $ "POST PARSE: " ++ show sk
+
+    let st = static (envof sk)
+    --putStrLn $ "POST STATIC: " ++ show st
+
+    syn <- runSMT solver (synthesize (cmd_opts args) (envof st))
+    case syn of
+      Nothing -> fail "sketch not satisfiable"
+      Just v -> putStrLn (pretty v)
 
--- sketch/utils/make.tcl
+++ sketch/utils/make.tcl
@@ -12,8 +12,8 @@ proc hrun {args} {
 # Run from the sketch/ directory.
 hrun mkdir -p build/release build/profile
 
-#hrun happy src/Grammar.y -o build/profile/Grammar.hs
-#hrun smten --make -o build/profile/sketch -prof -fprof-auto-top -ibuild/profile -isrc -hidir build/profile -odir build/profile src/sketch.hs -fwarn-unused-imports
+hrun happy src/Grammar.y -o build/profile/Grammar.hs
+hrun smten --make -o build/profile/sketch -prof -fprof-auto-top -ibuild/profile -isrc -hidir build/profile -odir build/profile src/sketch.hs -fwarn-unused-imports
 
 hrun happy src/Grammar.y -o build/release/Grammar.hs
 hrun smten --make -o build/release/sketch -ibuild/release -isrc -hidir build/release -odir build/release src/sketch.hs -fwarn-unused-imports
