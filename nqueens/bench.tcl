97427cc065ebb212764883da03031e7cba1f1d30
Richard Uhler <ruhler@csail.mit.edu>
2014-02-03 11:30:26 -0500

    Update performance gathering.

--- nqueens/bench.tcl
+++ nqueens/bench.tcl
@@ -23,6 +23,11 @@ proc cfgrep {cfg} {
     return [lindex $cfg 2]
 }
 
+proc elem {x l} {
+    set pos [lsearch $l $x]
+    return [expr [lsearch $l $x] != -1]
+}
+
 proc gendata {maxtimepertest configs} {
     # Output the configuration names
     foreach cfg $configs {
@@ -31,26 +36,39 @@ proc gendata {maxtimepertest configs} {
     puts ""
 
     # Increase n until all configurations are done.
+    # Done means either:
+    #    - it has taken longer than maxtimepertest to get an answer for some n
+    #    - or there have been back to back errors
     set done [list]
+    set errprev [list]
+    set errthis [list]
     for {set n 1} {[llength $done] < [llength $configs]} {incr n} {
         foreach cfg $configs {
-           # yices1 Bit 55 hits a seg fault, so skip it.
-           if {[cfgsolver $cfg] == "yices1" && [cfgrep $cfg] == "Bit" && $n == 55} {
+           if {[elem [cfgname $cfg] $done]} {
                puts -nonewline "NA "
-               continue
-           }
-
-           if {[lsearch $done [cfgname $cfg]] == -1} {
-               set t [mytime "exec ./nqueens -s [cfgsolver $cfg] -e [cfgrep $cfg] $n"]
-               puts -nonewline "$t "
-               if {$maxtimepertest < $t} {
-                   lappend done [cfgname $cfg]
-               }
            } else {
-               puts -nonewline "NA "
+               if { [catch {
+                       set t [mytime "exec ./nqueens -s [cfgsolver $cfg] -e [cfgrep $cfg] $n"]
+                       puts -nonewline "$t "
+                       if {$maxtimepertest < $t} {
+                           lappend done [cfgname $cfg]
+                       }}]} {
+                   # An error occurred
+                   puts -nonewline "NA "
+                   if {[elem [cfgname $cfg] $errprev]} {
+                       lappend done [cfgname $cfg]
+                   } else {
+                       lappend errthis [cfgname $cfg]
+                   }
+               }
            }
        }
        puts ""
+       set errprev $errthis
+       set errthis [list]
     }
+
+    # Output a blank line as indication that we finished.
+    puts ""
 }
 
