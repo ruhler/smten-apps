ac58079f83ae45b29b5fe8bc0146326739fb2d5b
Richard Uhler <ruhler@csail.mit.edu>
2014-02-23 09:46:44 -0500

    Made the BitOneHotCell less stupid.

--- sudoku/src/BitOneHotCell.hs
+++ sudoku/src/BitOneHotCell.hs
@@ -11,53 +11,25 @@ import Cell
 
 type BitOneHotCell = Bit 9
 
-c1 :: BitOneHotCell
-c1 = 1
-
-c2 :: BitOneHotCell
-c2 = 2
-
-c3 :: BitOneHotCell
-c3 = 4
-
-c4 :: BitOneHotCell
-c4 = 8
-
-c5 :: BitOneHotCell
-c5 = 16
-
-c6 :: BitOneHotCell
-c6 = 32
-
-c7 :: BitOneHotCell
-c7 = 64
-
-c8 :: BitOneHotCell
-c8 = 128
-
-c9 :: BitOneHotCell
-c9 = 256
-
 isValidCell :: BitOneHotCell -> Bool
-isValidCell c = elem c [c1, c2, c3, c4, c5, c6, c7, c8, c9]
+isValidCell c = c `bv_and` (c - 1) == 0
 
 join :: [BitOneHotCell] -> Bit 9
 join xs = foldl bv_or 0 xs
 
 instance Cell BitOneHotCell where
-    mkCell i = [c1, c2, c3, c4, c5, c6, c7, c8, c9] !! (i-1)
+    mkCell i = 1 `bv_shl` toEnum (i - 1)
 
-    deCell c = 
-           if c == c1 then 1
-      else if c == c2 then 2
-      else if c == c3 then 3
-      else if c == c4 then 4
-      else if c == c5 then 5
-      else if c == c6 then 6
-      else if c == c7 then 7
-      else if c == c8 then 8
-      else 9
-
+    deCell 0x1 = 1
+    deCell 0x2 = 2
+    deCell 0x4 = 3
+    deCell 0x8 = 4
+    deCell 0x10 = 5
+    deCell 0x20 = 6
+    deCell 0x40 = 7
+    deCell 0x80 = 8
+    deCell 0x100 = 9
+        
     freeCell = do
         x <- free_Bit
         assert (isValidCell x)
