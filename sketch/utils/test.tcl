052926ec62a571160ba9850d660a1b5118ed0520
Richard Uhler <ruhler@csail.mit.edu>
2014-03-03 14:58:02 -0500

    Reclassify some tests, because they don't work well with other solvers.

--- sketch/utils/test.tcl
+++ sketch/utils/test.tcl
@@ -1,17 +1,16 @@
 
-set ::SKETCH ./build/release/sketch
-#set ::SKETCH sketch
+set ::SKETCH [lindex $argv 0]
 
 # All tests of the form tests/*.sk should pass
 foreach x [glob tests/*.sk] {
     puts "$x ..."
-    exec $::SKETCH $x
+    exec {*}$::SKETCH $x
 }
 
 # All tests of the form tests/expectfail/*.sk should fail
 foreach x [glob tests/expectfail/*.sk] {
     puts "$x ..."
-    if {[catch "exec $::SKETCH $x"] == 0} {
+    if {[catch "exec {*}$::SKETCH $x"] == 0} {
         error "expected failure, but completed"
     }
 }
