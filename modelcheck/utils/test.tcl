b2a82d82faa0eac8f8b91e671d311bf573b177e6
Richard Uhler <ruhler@csail.mit.edu>
2014-03-07 14:18:27 -0500

    Only run a handful of tests for check target.

--- modelcheck/utils/test.tcl
+++ modelcheck/utils/test.tcl
@@ -1,12 +1,18 @@
 
-# All tests from tests/sat/*.aig should pass
-foreach x [glob tests/sat/*.aig] {
+# All tests from utils/tsat.txt should pass
+set fin [open "utils/tsat.txt" "r"]
+set tests [split [string trim [read $fin]]]
+close $fin
+foreach x $tests {
     puts "$x ..."
     exec ./utils/runsat $x
 }
 
-# All tests from tests/unsat/*.aig should be unsat
-foreach x [glob tests/unsat/*.aig] {
+# All tests from utils/tunsat.txt should be unsat
+set fin [open "utils/tunsat.txt" "r"]
+set tests [split [string trim [read $fin]]]
+close $fin
+foreach x $tests {
     puts "$x ..."
     exec ./utils/rununsat $x
 }
