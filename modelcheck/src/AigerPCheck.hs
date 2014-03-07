c177a3421978a25f3d4dfb4bca81615aaf5846a4
Richard Uhler <ruhler@csail.mit.edu>
2014-03-07 13:55:22 -0500

    Minor code cleanup. Add timeout to bench.tcl.

--- modelcheck/src/AigerPCheck.hs
+++ modelcheck/src/AigerPCheck.hs
@@ -4,7 +4,9 @@ module AigerPCheck (
   ) where
 
 import Smten.Prelude
+import Smten.Control.Monad
 import Smten.Data.Array
+import Smten.Data.Functor
 import Smten.Symbolic
 import Smten.Symbolic.SMT
 import Aiger
@@ -13,23 +15,20 @@ import PCheck
 data AigerState = AigerState {
   as_input :: Vector,
   as_state :: Vector
-}
-
-instance Eq AigerState where
-  (==) a b = (as_input a == as_input b) && (as_state a == as_state b)
+} deriving (Eq)
 
 aigermodel :: Aiger -> Model AigerState
 aigermodel aig = Model {
-  _I = \as -> all (== False) (elems (as_state as)),
-  _T = \a1 a2 ->
-          let (_, ns) = aig_step aig (as_input a1) (as_state a1)
-          in ns == (as_state a2)
+  _I = \as -> {-# SCC "AigerModel_I" #-} all not (elems (as_state as)),
+  _T = \a1 a2 -> {-# SCC "AigerModel_T" #-}
+           case aig_step aig (as_input a1) (as_state a1) of
+              (_, ns) -> ns == as_state a2
 }
 
 aigerfreestate :: Aiger -> Symbolic AigerState
 aigerfreestate aig = do
-   inputs <- sequence (replicate (aig_num_inputs aig) free_Bool)
-   state <- sequence (replicate (aig_num_latches aig) free_Bool)
+   inputs <- replicateM (aig_num_inputs aig) free_Bool
+   state <- replicateM (aig_num_latches aig) free_Bool
    return $ AigerState {
         as_input = listArray (1, aig_num_inputs aig) inputs,
         as_state = listArray (1, aig_num_latches aig) state
@@ -41,7 +40,5 @@ aigerpred aig l = \as -> not (aig_eval aig (as_input as) (as_state as) l)
 aigercheck :: Aiger -> Literal -> SMT (Maybe [Vector])
 aigercheck aig l = do
   mstates <- pcheck (aigermodel aig) (aigerfreestate aig) (aigerpred aig l)
-  return $ do
-     states <- mstates
-     return (map as_input states)
+  return $ (map as_input <$> mstates)
 
--- modelcheck/utils/bench.tcl
+++ modelcheck/utils/bench.tcl
@@ -1,15 +1,20 @@
 
 set ::runspertest 1
+set ::timeout 30
 
 # Return the average time in seconds to run the given script.
 proc mytime {script} {
     return [expr [lindex [time $script $::runspertest] 0] / 1.0e6]
 }
 
+proc withtimeout {args} {
+    catch "exec timeout ${::timeout}s $args"
+}
+
 puts "  saiger pdtrav"
 foreach x [glob tests/sat/*.aig tests/unsat/*.aig] {
-    set saiger [mytime "exec ./utils/run $x"]
-    set pdtrav [mytime "exec ./utils/pdtrav $x"]
+    set saiger [mytime "withtimeout ./utils/run $x"]
+    set pdtrav [mytime "withtimeout ./utils/pdtrav $x"]
     puts "$x $saiger $pdtrav"
 }
 
