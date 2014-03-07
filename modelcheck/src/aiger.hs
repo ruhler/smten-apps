39f7135453fe7093aef17f4fa9a85227c6292872
Richard Uhler <ruhler@csail.mit.edu>
2014-03-07 14:08:19 -0500

    Accept solver as a command line argument.

--- modelcheck/src/aiger.hs
+++ modelcheck/src/aiger.hs
@@ -2,18 +2,52 @@
 import Smten.Prelude
 import Smten.Data.Array
 import Smten.Symbolic.SMT
+import Smten.Symbolic.Solver.Debug
+import Smten.Symbolic.Solver.MiniSat
+import Smten.Symbolic.Solver.STP
+import Smten.Symbolic.Solver.Z3
+import Smten.Symbolic.Solver.Yices1
 import Smten.Symbolic.Solver.Yices2
+import Smten.System.Environment
+import Smten.System.Exit
 import Aiger
 import AigerPCheck
 
+usage :: String
+usage = "aiger [-d debug] [-s yices1 | yices2 | stp | z3 | minisat] < FILE"
+
+lookuparg :: String -> [String] -> Maybe String
+lookuparg k m = 
+  case dropWhile ((/=) k) m of
+     (_:x:_) -> Just x
+     _ -> Nothing
+
 main :: IO ()
 main = do
+  args <- getArgs
+  if "--help" `elem` args
+     then putStrLn usage >> exitSuccess
+     else return ()
+
+  basesolver <- case lookuparg "-s" args of
+                   Just "yices1" -> return yices1
+                   Just "yices2" -> return yices2
+                   Just "stp" -> return stp
+                   Just "z3" -> return z3
+                   Just "minisat" -> return minisat
+                   Just x -> fail $ "Unknown solver: " ++ x ++ ".\n" ++ usage
+                   Nothing -> return yices2
+
+  solver <- case lookuparg "-d" args of
+                Just fn -> debug fn basesolver
+                Nothing -> return basesolver
+
   txt <- getContents
   let aig = readAsciiAiger txt
 
   -- Note: we only look for the first output.
   -- Presumably we need to check for them all?
-  result <- runSMT yices2 $ aigercheck aig (aig_outputs aig ! 1)
+  result <- runSMT solver $ aigercheck aig (aig_outputs aig ! 1)
   case result of
     Nothing -> putStrLn "0\n."
     Just v -> do
