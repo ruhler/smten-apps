bdc92315be038b825fe71f46f8076765ea702639
Richard Uhler <ruhler@csail.mit.edu>
2014-05-29 14:02:08 -0400

    Re-organize benchmarks.
    Now we only run check on the unit tests. Be careful of breaking other things.

--- sketch/utils/test.tcl
+++ sketch/utils/test.tcl
@@ -1,14 +1,14 @@
 
 set ::SKETCH [lindex $argv 0]
 
-# All tests of the form tests/*.sk should pass
-foreach x [glob tests/*.sk] {
+# All tests of the form benchmarks/unit/*.sk should pass
+foreach x [glob benchmarks/unit/*.sk] {
     puts "$x ..."
     exec {*}$::SKETCH $x
 }
 
-# All tests of the form tests/expectfail/*.sk should fail
-foreach x [glob tests/expectfail/*.sk] {
+# All tests of the form benchmarks/unit/expectfail/*.sk should fail
+foreach x [glob benchmarks/unit/expectfail/*.sk] {
     puts "$x ..."
     if {[catch "exec {*}$::SKETCH $x"] == 0} {
         error "expected failure, but completed"
