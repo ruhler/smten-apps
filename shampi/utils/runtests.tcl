09262baca1318436ff80e55df03091e86d8469b5
Richard Uhler <ruhler@csail.mit.edu>
2014-06-09 14:11:48 -0400

    Sort the shampi unit tests when running.
    Otherwise they might not match the expected result, just because they are
    out of order.

--- shampi/utils/runtests.tcl
+++ shampi/utils/runtests.tcl
@@ -1,7 +1,7 @@
 
 set ::hampi [lindex $argv 0]
 
-foreach x [glob "tests/unit/*.hmp"] {
+foreach x [lsort [glob "tests/unit/*.hmp"]] {
     catch {exec $::hampi -t 3 $x ">@" stdout "2>@" stdout}
 }
     
